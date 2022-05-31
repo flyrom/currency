334129ca61947ad0d861a177270a4eb7f89d886b
Romain <grose.romain@gmail.com>
2022-05-31 16:39:04 +0200

    currency

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Body =() => {
+    const list = ['€', '$', '£'];
+        return(
+            <div>
+                <select></select>
+            </div>
+        )
+}
+
+export default Body;
